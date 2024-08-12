6e2f55005fac3ba1e094ff57ded7d0d73cf42141
ujiuji94 <ujitaketigers32702@gmail.com>
2024-08-12 17:30:43 +0900

    Profile

---
+++ development/src/app/profile/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+export default function Profile() {
+    return (
+      <div style={{border: 'solid red'}}>
+      <h1> Profile Page
+              
+      </h1>
+      <p>趣味：野球観戦，全国の都市巡り</p>
+      <p>好物：台湾まぜそば</p>
+      </div>
+      
+
+
+
+    
+    )
+}
+    // (
+  //     <div>
+  //       <Head>
+  //         <title>Profile Page</title>
+          
+        
+  //       </Head>
+  //       <h1>Hello World</h1>
+
+
+        
+  //     </div>
+  //   )
+  // }
